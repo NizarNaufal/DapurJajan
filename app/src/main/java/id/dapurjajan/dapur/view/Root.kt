629b551030600334b523fa405ccd50afed1e9f05
nizarnaufal <nizarnfl21@gmail.com>
2020-11-29 13:16:27 +0700

    Fix Splash Screen And RubberLoadingView

---
+++ app/src/main/java/id/dapurjajan/dapur/view/Root.kt
@@ -0,0 +1,28 @@
+package id.dapurjajan.dapur.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.greenfrvr.rubberloader.RubberLoaderView
+import id.kelompokbeta.wisato.R
+import id.kelompokbeta.wisato.support.showActivity
+import id.kelompokbeta.wisato.view.intro.WelcomeSlider
+import java.util.*
+
+
+class Root : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        (findViewById<View>(R.id.loader1) as RubberLoaderView).startLoading()
+        val myTimer = Timer()
+        myTimer.schedule(object : TimerTask() {
+            override fun run() {
+                // If you want to modify a view in your Activity
+                runOnUiThread {
+                    showActivity(WelcomeSlider::class.java)
+                }
+            }
+        }, 500)
+    }
+}
